1bb7a4b93274e8488482c62e511940e783b2cc22
kelleyrw <kelleyrw@users.noreply.github.com>
2021-10-26 12:27:33 -0700

    added deploy scripts

--- project/tests/test_ping.py
+++ project/tests/test_ping.py
@@ -18,4 +18,4 @@ def test_env():
 def test_ping(test_app):
     response = test_app.get("/ping")
     assert response.status_code == 200
-    assert response.json() == {"environment": "dev", "ping": "pong!", "testing": True}
+    assert response.json() == {"environment": "dev", "ping": "pong!!", "testing": True}
